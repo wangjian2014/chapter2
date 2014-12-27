02bef82021e2090b7d5b97eef0b32ea941438a39
jianwang <wangjian3@lianchuang.com>
2014-12-27 17:49:59 +0800

    first commit

---
+++ src/com/baobaotao/web/LoginCommand.java
@@ -0,0 +1,35 @@
+package com.baobaotao.web;
+
+import java.util.Date;
+
+public class LoginCommand {
+	private String userName;
+
+	private String password;
+
+	private Date date;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+}
